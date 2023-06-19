0780945fcdd781d42365e6f179e6d24c53e6d9c1
achrafOuk <achraf.crispy@gmail.com>
2023-06-19 16:28:25 +0100

    add authors input in book form

--- resources/views/components/authors-input.blade.php
+++ resources/views/components/authors-input.blade.php
@@ -1,30 +1,19 @@
 @props(['authors','selected_authors'])
 
-<div x-data="{ selected_authors: {{ $selected_authors ?? '[]' }},count:1">
-  <label class="mb-2 block text-sm font-bold text-gray-700" > Authors: </label>
-  <template x-for="i in count" >
-    <div class="flex flex-row items-center " >
-        <div class="w-1/2 flex flex-col mr-2" >
-            <input type="text" 
-            class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" 
-            name="authors[]"
-            x-bind:value="selected_authors[i]"
-            >
-            <datalist id="authors">
-                @foreach( $authors as $author)
-                <option value="{{$author->name}}"></option>
-                @endforeach
-            </datalist>
+<div class="mb-4" x-data="{ selected_authors: {{ $selected_authors ?? '[]' }},count:1, }">
+    <label class="mb-2 block text-sm font-bold text-gray-700" >Authors: </label>
+    <template x-for="i in count" >
+        <div class="flex mb-2">
+            <input id="category" name="category" list="categoryOptions"
+            type="text"
+            class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            required>
+            <button type="button"
+            class="px-4 py-2 ml-2 text-white bg-purple rounded-md focus:outline-none focus:ring "
+            x-on:click="count++" >+</button>
+            <button type="button"
+            class="px-4 py-2 ml-2 text-white bg-purple rounded-md focus:outline-none focus:ring "
+            x-show="count>1" x-on:click="count--" >-</button>
         </div>
-        <div class="flex flex-row mr-2 mt-10 ">
-            <button type="button" x-on:click="count++"  class="flex-shrink-0 px-4 py-2 text-white bg-yellow mr-2 font-bold">
-                +
-            </button>
-            <button type="button" x-show="count>1" x-on:click="count--"  class="flex-shrink-0 px-4 py-2 text-white bg-yellow font-bold">
-                -
-            </button>
-
-        </div>
-    </div>
-  </template>
+    </template>
 </div>
--- resources/views/components/book-form.blade.php
+++ resources/views/components/book-form.blade.php
@@ -29,16 +29,6 @@
           {{$description}}
         </textarea>
         </div>
-        {{-- <div class="mb-4">
-          <label class="mb-2 block text-sm font-bold text-gray-700" for="language"> Authors: </label>
-          <input type="text" name="authors[]"  list="authors" class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none" />
-          <datalist id="authors">
-            @foreach( $authors as $author)
-              <option value="{{$author->name}}"></option>
-            @endforeach
-
-          </datalist>
-        </div> --}}
 
         {{-- 
         <div class="mb-4">
